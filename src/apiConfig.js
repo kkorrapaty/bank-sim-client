a79fbcfc4ec0f1a9d850ca08875a8b2abea32cec
kkorrapaty <kkorrapatyjr@gmail.com>
2020-08-06 17:03:15 -0400

    Deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://bank-sim-server.herokuapp.com',
   development: 'http://localhost:8000'
 }
 
